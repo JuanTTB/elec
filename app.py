d9fbf7d5ebcd896c88c0beae8a8501d89ec8fe34
JuanTTB <jtzz018000@gmail.com>
2024-05-24 02:59:59 -0500

    Porfinx2

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import config
+import os
 
 app = Flask(__name__)
 
@@ -56,4 +57,5 @@ def delete_task(task_id):
         return jsonify({'error': 'Task not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
